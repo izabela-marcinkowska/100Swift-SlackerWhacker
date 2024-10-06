11bea2e386514e1cb70ad9c676c95cbb0acce8f8
Devy <izabela.marcinkowska@outlook.com>
2024-10-06 17:04:20 +0200

    first commit

---
+++ SlackerWhacker/SlackerWhackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlackerWhackerApp.swift
+//  SlackerWhacker
+//
+//  Created by Izabela Marcinkowska on 2024-10-06.
+//
+
+import SwiftUI
+
+@main
+struct SlackerWhackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
