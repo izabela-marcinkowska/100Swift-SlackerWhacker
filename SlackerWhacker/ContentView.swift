a141f02cc84736aaf863f1437d258310d3b5c37e
Devy <izabela.marcinkowska@outlook.com>
2024-10-08 10:43:28 +0200

    feat: Move form to separate view

--- SlackerWhacker/ContentView.swift
+++ SlackerWhacker/ContentView.swift
@@ -9,26 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var myHabits = Habits()
-    @State private var name: String = ""
-    @State private var description: String = ""
+    
      var body: some View {
-         List {
-             Section ("Habits") {
-                 ForEach(myHabits.habits) { habit in
-                     HStack {
-                         VStack(alignment: .leading) {
-                             Text(habit.name).font(.headline)
-                             Text(habit.description)
+         NavigationStack {
+             List {
+                 Section ("Habits") {
+                     ForEach(myHabits.habits) { habit in
+                         HStack {
+                             VStack(alignment: .leading) {
+                                 Text(habit.name).font(.headline)
+                                 Text(habit.description)
+                             }
                          }
                      }
                  }
              }
+             .navigationTitle("SlackerWhacker")
+             .toolbar {
+                 NavigationLink("Add habit") {
+                     HabitForm(myHabits: myHabits)
+                 }
+             }
          }
-        Form {
-            TextField("Name", text: $name)
-            TextField("Description", text: $description)
-            Button("Add habit", action: {myHabits.addHabit(name: name, description: description)})
-        }
+         
     }
 }
 
---
+++ SlackerWhacker/HabitForm.swift
@@ -0,0 +1,30 @@
+//
+//  Form.swift
+//  SlackerWhacker
+//
+//  Created by Izabela Marcinkowska on 2024-10-08.
+//
+
+import SwiftUI
+
+struct HabitForm: View {
+    @State private var name: String = ""
+    @State private var description: String = ""
+    @ObservedObject var myHabits: Habits
+    
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        Form {
+            TextField("Name", text: $name)
+            TextField("Description", text: $description)
+            Button("Add habit", action: {
+                myHabits.addHabit(name: name, description: description)
+                dismiss()
+            })
+        }
+    }
+}
+
+#Preview {
+    HabitForm(myHabits: Habits())
+}
